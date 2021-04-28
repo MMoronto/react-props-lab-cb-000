702c8993af52475ab5383395af39f51b58329638
MOBOLA MORONTO <t2wycemo@gmail.com>
2021-04-28 19:13:57 +0000

    Automatically backed up by Learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -2,8 +2,12 @@ class Spaceship extends React.Component {
   render() {
     return (
         <div className="space-ship">
-          <h2>{this.props.name}</h2>
-          <small>Speed: {this.props.speed}
+          <h2>Name: {this.props.name}</h2>
+          <h1>Speed: {this.props.speed}</h1>
+          <h1>Has Rockets: {this.props.hasRockets}</h1>
+          <h1>Colors: {this.props.colors}</h1>
+          
+          
         </div>
       );
   }
