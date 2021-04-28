6c1ee6227f38c15e3637ed9d2461cdc924de4a09
MOBOLA MORONTO <t2wycemo@gmail.com>
2021-04-28 19:27:18 +0000

    Automatically backed up by Learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -4,10 +4,10 @@ class Spaceship extends React.Component {
   render() {
     return (
         <div className="space-ship">
-          <h2>Name: {this.props.name}</h2>
-          <h1>Speed: {this.props.speed}</h1>
-          <h1>Has Rockets: {this.props.hasRockets}</h1>
-          <h1>Colors: {this.props.colors}</h1>
+          <h1>Name: {this.props.name}</h1>
+          <p>Speed: {this.props.speed}</p>
+          <p>Has Rockets: {this.props.hasRockets}</p>
+          <p>Colors: {this.props.colors}</p>
           
           
         </div>
@@ -20,3 +20,5 @@ Spaceship.defaultProps = {
   hasRockets: 'false',
   colors: ['black', 'red']
 }
+
+export default Spaceship;
